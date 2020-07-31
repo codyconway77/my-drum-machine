9bd604c8d825b92d20b842fafa5597d413cd4597
Cody Conway <codyconway77@gmail.com>
2020-07-31 15:48:29 -0700

    Everything funcional, still need style

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import DrumPad from './DrumPad';
 
 const sounds = [
-  {id: 'Heater-1', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'} ,
-  {id: 'Heater-2', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'} ,
-  {id: 'Heater-3', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'} ,
-  {id: 'Heater-4', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'} ,
-  {id: 'Heater-6', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3 '} ,
-  {id: 'Dsc_Oh', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'} ,
-  {id: 'Kick_n_Hat', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'} ,
-  {id: 'RP4_KICK_1', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'} ,
-  {id: 'Cev_H2', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'} ,
+  {keyCode:81, id: 'Heater-1', letter: 'Q', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'} ,
+  {keyCode:87, id: 'Heater-2', letter: 'W', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'} ,
+  {keyCode:69, id: 'Heater-3', letter: 'E', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'} ,
+  {keyCode:65, id: 'Heater-4', letter: 'A', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'} ,
+  {keyCode:83, id: 'Heater-6', letter: 'S', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3 '} ,
+  {keyCode:68, id: 'Dsc_Oh', letter: 'D', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'} ,
+  {keyCode:90, id: 'Kick_n_Hat', letter: 'Z', src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'} ,
+  {keyCode:88, id: 'RP4_KICK_1', letter: 'X', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'} ,
+  {keyCode:67, id: 'Cev_H2', letter: 'C', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'} ,
 ]
 
 export class App extends React.Component {
@@ -22,25 +22,24 @@ export class App extends React.Component {
     drumPad: sounds,
     display: ''
   }
-  handleClick = () => {
-    this.audio.play()
-    this.audio.currentTime = 0
+  handleDisplay = string => {
     this.setState({
-        display: this.props.id
+        display: string
     })
 }
   render(){
     return (
       <div className="app">
         <div id="drum-machine">
-          <div id="display">
+          <div id="display">{this.state.display}
           </div>
           {this.state.drumPad.map(s => (
             <DrumPad
+              keyCode={s.keyCode}
               id={s.id}
               letter={s.letter}
               src={s.src}
-              action={this.handleClick}
+              handleDisplay={this.handleDisplay}
             />  
           ))}
         </div>
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 
 export class DrumPad extends Component {
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress)
+    }
+    componentWillUnmount() {
+
+    }
+
     handleClick = () => {
         this.audio.play()
         this.audio.currentTime = 0
+        this.props.handleDisplay(this.props.id)
     }
+    handleKeyPress = event => {
+        if (event.keyCode === this.props.keyCode) {
+            this.audio.play()
+            this.audio.currentTime = 0
+            this.props.handleDisplay(this.props.id)
+        }
+    }
+    handleDisplay = string => {
+        this.setState({
+            display: string
+        })
+    }
+
     render() {
         return (
             <div
             className="drum-pad" 
             id={this.props.id}
+            onKeyPress={this.handleKeyPress}
             onClick={this.handleClick}>
                 <p>{this.props.letter}</p>
                 <audio 
